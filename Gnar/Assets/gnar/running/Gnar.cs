3ea6399d10ea06aef272e3cb5ca5f406a560384a
OneTrickSupport <63346082+OneTrickSupport@users.noreply.github.com>
2020-09-22 16:22:14 +0200

    Gnar project

---
+++ Gnar/Assets/gnar/running/Gnar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gnar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update() {
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal / 50;
+
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, 10.0f));
+
+    }
+}
